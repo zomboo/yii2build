c04847162c1e388164ea2dcf9fa3c12e3a47c00d
zomboo <zomboo@gmail.com>
2016-02-29 16:38:44 +0800

    20160229

---
+++ frontend/views/profile/_form.php
@@ -0,0 +1,26 @@
+<?php
+use yii\helpers\Html;
+use yii\widgets\ActiveForm;
+use yii\jui\DatePicker;
+/**
+* @var yii\web\View $this
+* @var app\models\Profile $model
+* @var yii\widgets\ActiveForm $form
+*/
+?>
+<div class="profile-form">
+<?php $form = ActiveForm:: begin(); ?>
+<?= $form-> field( $model , 'first_name' ) -> textInput([ 'maxlength' => 45]) ?>
+<?= $form-> field( $model , 'last_name' ) -> textInput([ 'maxlength' => 45]) ?>
+<br/>
+<?php echo $form-> field( $model , 'birthdate' ) -> widget(DatePicker:: className(),
+[ 'clientOptions' => [ 'dateFormat' => 'yy-mm-dd' ]]); ?>
+<br/>
+<?= $form-> field( $model , 'gender_id' ) -> dropDownList( $model -> genderList,
+[ 'prompt' => 'Please Choose One' ]); ?>
+<div class="form-group">
+<?= Html :: submitButton( $model -> isNewRecord ? 'Create' : 'Update' ,
+[ 'class' => $model -> isNewRecord ? 'btn btn-success' : 'btn btn-primary' ]) ?>
+</div>
+<?php ActiveForm:: end(); ?>
+</div>
